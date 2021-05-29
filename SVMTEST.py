b4419a2a6b3e595f2ad6361866ba5e526001789c
fproks <fproks@126.com>
2021-05-29 20:07:39 +0800

    update test file

--- SVMTEST.py
+++ SVMTEST.py
@@ -1,4 +1,4 @@
-from  sklearn.svm import SVC
+from sklearn.svm import SVC
 from elm import BvsbClassifier, BvsbUtils
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
@@ -6,29 +6,23 @@ from elm import elmUtils
 
 from sklearn.preprocessing import StandardScaler
 
-
 import time
+
 stdsc = StandardScaler()
-data=datasets.fetch_covtype()
-data.data=stdsc.fit_transform(data.data)/16.0
-data.data,_, data.target,_=train_test_split(data.data,data.target,test_size=0.7)
-label_size=0.05
+data = datasets.fetch_covtype()
+data.data = stdsc.fit_transform(data.data) / 16.0
 
-acc=0
-print(data.data.shape)
-#print(data.target.shape)
+label_size = 0.05
 
+t1 = time.perf_counter_ns()
 
-t1=time.perf_counter_ns()
+train, test = elmUtils.splitData(data.data, data.target, 1 - label_size, True)
+svm = SVC()
+svm.fit(train[0], train[1])
+tmp_acc = svm.score(test[0], test[1])
+print(f'SVM 正确率为: {tmp_acc}')
 
-for i in range(10):
-    train, test = elmUtils.splitData(data.data, data.target, 1 - label_size, True)
-    svm=SVC()
-    svm.fit(train[0],train[1])
-    tmp_acc=svm.score(test[0], test[1])
-    print(f'SVM 正确率为: {tmp_acc}')
-    acc+=tmp_acc
-t2=time.perf_counter_ns()
+t2 = time.perf_counter_ns()
 
-print(f'SVM平均正确率为{acc/10}')
-print(f'SVM用时 {(t2-t1)/1000/1000/10}毫秒')
+print(f'SVM平均正确率为{tmp_acc}')
+print(f'SVM用时 {(t2 - t1) / 1000 / 1000 / 10}毫秒')
